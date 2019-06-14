a95f19895aa4c0f7c2bd2ba6636323fc86530733
Artem <inste@icloud.com>
2019-06-14 20:46:47 +0300

    Initial Commit

---
+++ UICollectionViewReorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewReorder
+//
+//  Created by Artem Karmaz on 14.06.2019.
+//  Copyright © 2019 Artem Karmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
